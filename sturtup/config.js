378764b9309f2e37578ff058aed931ce594d4ac4
jadidihosein68 <jadidi.hosein@gmail.com>
2023-06-05 21:02:04 +0800

    just for deploy

--- sturtup/config.js
+++ sturtup/config.js
@@ -1,8 +1,6 @@
 const config = require('config');
 require('dotenv').config();
 
-
-
 module.exports=function(){
     if (!process.env.jwtPrivateKey) {
        throw new Error("fatal, jwtPrivateKey is not defined !");
