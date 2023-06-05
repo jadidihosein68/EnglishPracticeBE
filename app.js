d8e6e10dced24519dd0850c3963a0997bb989981
jadidihosein68 <jadidi.hosein@gmail.com>
2023-06-05 20:34:41 +0800

    remove ugly code

--- app.js
+++ app.js
@@ -9,33 +9,25 @@ require('./sturtup/prod')(app);
 require('./sturtup/routes')(app);
 require('./sturtup/db')();
 
-
-
-
 process.on('uncaughtException', (ex)=>{ // capture un handeled exception on the scope out of express
   winstonlogger.error(ex.message,ex);
 });
-
 
 process.on('unhandledRejection', (ex)=>{ // capture un handeled exception on the scope out of express
   winstonlogger.error(ex.message,ex);
 });
 
-
 //////////////////
 app.get('/api/ping', (req, res) => { // test api 
   res.send("I am alive");
-
 });
 
 //////////////////
 
 const port = process.env.PORT || 3000;
 
-
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-
 
  // endpoint http://localhost:3000/getvocabulary
